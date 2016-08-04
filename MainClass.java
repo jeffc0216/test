6fa756f97d3015730c7a081d44df7f9097ad3cfe
Jeff Chen <jeffc0216@gmail.com>
2016-08-04 08:27:57 +0800

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,18 @@
+package ch04test;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int score = 85;
+		if(score >= 85){
+			System.out.println("win 10000");
+		}else if(score >= 75){
+			System.out.println("win 5000");
+		}else{
+			System.out.println("study harder");
+		}
+		
+	}
+
+}
